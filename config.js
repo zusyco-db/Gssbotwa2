fbed718b49ac68151de0b5eebf46913ecb420a4c
zusyco-db <zusycofiles@hotmail.com>
2024-06-27 11:08:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,25 +7,25 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "JUMATECHN";
+global.owner = ['254723616685'];
+global.premium = ['254723616685'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "FIGA NINTY";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
+global.available = true 
 global.autoTyping = false
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false 
+global.autoBlock = false 
  
 /*REPLY MESSAGE*/
 global.mess = {
